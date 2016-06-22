4f4e8754bca3b3449c69c9518de38b633ea234e1
violetaGitHub <vsanchezm@codicesoftware.com>
2016-06-22 11:17:00 +0200

    Create test.cs

---
+++ test.cs
@@ -0,0 +1,81 @@
+using System.Collections.Generic;
+using System.IO;
+
+namespace DatalayerFs.Serializers
+{
+    internal class FindByRepositoryId : IReadCondition<RepositoryRow>
+    {
+        internal FindByRepositoryId(uint id)
+        {
+            mTargetId = id;
+        }
+
+        bool IReadCondition<RepositoryRow>.Matches(ref RepositoryRow current)
+        {
+            return current.Id == mTargetId;
+        }
+
+        uint mTargetId;
+    }
+
+    internal class FindByRepositoryName : IReadCondition<RepositoryRow>
+    {
+        internal FindByRepositoryName(string name)
+        {
+            mTargetName = name;
+        }
+
+        bool IReadCondition<RepositoryRow>.Matches(ref RepositoryRow current)
+        {
+            return current.Name == mTargetName;
+        }
+
+        string mTargetName;
+    }
+
+    internal static class RepositorySerializer
+    {
+        internal static List<RepositoryRow> Read(
+            IReadCondition<RepositoryRow> condition, string path)
+        {
+            List<RepositoryRow> result = new List<RepositoryRow>();
+            RepositoryRow row = new RepositoryRow();
+
+            using (BinaryReader reader = IOBuilder.CreateBinaryReader(path))
+            while (reader.BaseStream.Position < reader.BaseStream.Length)
+            {
+                Read(ref row, reader);
+                if (condition == null || condition.Matches(ref row))
+                    result.Add(row);
+            }
+            return result;
+        }
+
+        internal static RepositoryRow ReadFirst(
+            IReadCondition<RepositoryRow> condition, string path)
+        {
+            RepositoryRow row = new RepositoryRow();
+
+            using (BinaryReader reader = IOBuilder.CreateBinaryReader(path))
+            while (reader.BaseStream.Position < reader.BaseStream.Length)
+            {
+                Read(ref row, reader);
+                if (condition == null || condition.Matches(ref row))
+                    return row;
+            }
+
+            return RepositoryRow.Empty;
+        }
+
+        static void Read(ref RepositoryRow row, BinaryReader reader)
+        {
+            row.Id = reader.ReadUInt32();
+            row.ModuleId = reader.ReadUInt32();
+            row.ParentModuleId = reader.ReadUInt32();
+            row.Name = reader.ReadString();
+            row.OwnerId = reader.ReadUInt32();
+            row.GuidHigh = reader.ReadInt64();
+            row.GuidLow = reader.ReadInt64();
+        }
+    }
+}
